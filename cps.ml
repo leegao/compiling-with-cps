9814b98cbf37712f290e36a99f315b8649033856
Lee Gao <leeyuan123@yahoo.com>
2013-04-14 19:02:54 -0400

    Implemented let propagation in IL1

--- cps.ml
+++ cps.ml
@@ -53,11 +53,33 @@ and cp_v (v:Il1.v) : Il1.v =
   | Il1.Fun (x,k,c) -> Il1.Fun (x,k,cp_c c)
   | Il1.Lam (x,c) -> Il1.Lam (x,cp_c c)
   | _ -> v
+
+let rec lp_c (c:Il1.c) : Il1.c =
+  match c with
+  | Il1.Let(x,e',c') ->
+    Il1.Let(x, lp_e e', lp_c c')
+  | Il1.Call(v0,v1,v2) ->
+    Il1.Call(lp_v v0, lp_v v1, lp_v v2)
+  | Il1.App(Il1.Lam(x,c'), v) ->
+    (*Let Propagate*)
+    Il1.Let(x,Il1.Val v, c')
+  | Il1.App(v0,v1) ->
+    Il1.App(lp_v v0, lp_v v1)
+and lp_e (e:Il1.e) : Il1.e =
+  match e with
+  | Il1.Val(v) -> Il1.Val(lp_v v)
+  | Il1.Plus(v1,v2) -> Il1.Plus(lp_v v1, lp_v v2)
+and lp_v (v:Il1.v) : Il1.v = 
+  match v with
+  | Il1.Fun (x,k,c) -> Il1.Fun (x,k,lp_c c)
+  | Il1.Lam (x,c) -> Il1.Lam (x,lp_c c)
+  | _ -> v
     
 (* Implement this! *)
 let translate(e: exp): tprog =
   let c = translate1 e Il1.Halt in
   let c = cp_c c in
+  let c = lp_c c in
   match e with
     _ -> raise (Fail "Implement me!")
 
